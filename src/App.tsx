f141bffa60b5ca491ee60acb5708c804c30675e3
Brainybrian316 <brainybrian316@gmail.com>
2022-10-28 15:54:02 -0400

    setting up search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			Search:
+			<input type="search" />
+			<br />
+			<br />
+			<form>
+				New Item: <input type="text" />
+				<button>Add</button>
+			</form>
+			<h3>Items:</h3>
+		</>
 	);
 }
 
