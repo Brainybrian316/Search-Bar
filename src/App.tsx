1b68e1fb9f5b7bfdec9b872e88658cc2231ce256
Brainybrian316 <brainybrian316@gmail.com>
2022-10-28 16:43:11 -0400

    functionality set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
+import React, { useState, useRef } from 'react';
+
 function App() {
+	const [items, setItems] = useState([]);
+	const inputRef = useRef();
+
+	function onSubmit(e: React.SyntheticEvent) {
+		e.preventDefault();
+		const value = inputRef.current.value;
+		if (value === '') {
+			return;
+		} else {
+			setItems((prev) => {
+				return [...prev, value];
+			});
+			inputRef.current.value = '';
+		}
+	}
+
 	return (
 		<>
 			Search:
 			<input type="search" />
 			<br />
 			<br />
-			<form>
-				New Item: <input type="text" />
-				<button>Add</button>
+			<form onSubmit={onSubmit}>
+				New Item: <input type="text" ref={inputRef} />
+				<button type="submit">Add</button>
 			</form>
 			<h3>Items:</h3>
+			{items.map((item: string) => (
+				<div>{item}</div>
+			))}
 		</>
 	);
 }
